6c94472370b5ce341faf402a744ad116f01bfd5b
Abdul Gafeeth Dramat <abdul@skywalkinnovations.co.za>
2024-08-15 20:20:54 +0200

    Setup react + vite + shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import {ReactElement} from "react";
+import {Button} from "@/components/ui/button.tsx";
+
+export const App = (): ReactElement => {
+
+    return (
+        <div>
+            <Button>Test</Button>
+        </div>
+    )
+}
